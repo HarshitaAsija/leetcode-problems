addec6a207d82b4fb752812e01e1fb2db3b6963f
Harshita Asija <asijaharshita@gmail.com>
2026-07-07 03:22:09 +0530

    Time: 0 ms (100%), Space: 28.2 MB (67.06%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int majorityElement(vector<int>& nums) {
 
@@ -18,5 +18,32 @@ public:
         }
 
         return -1;
+    }
+};*/
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for(int num : nums)
+        {
+            if(count == 0)
+            {
+                candidate = num;
+                count = 1;
+            }
+            else if(num == candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+
+        return candidate;
     }
 };
